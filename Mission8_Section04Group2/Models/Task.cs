3de22b33549e250df268d5dc8574b438d9a2881a
Madi Erickson <emadisom@byu.edu>
2025-02-24 14:49:35 -0700

    save

--- Mission8_Section04Group2/Models/Task.cs
+++ Mission8_Section04Group2/Models/Task.cs
@@ -8,6 +8,10 @@ namespace Mission8_Section04Group2.Models
         public int Task {  get; set; }
         public DateTime? DueDate {  get; set; }
         [Required]
-        public int Quadrant 
+        public int Quadrant {  get; set; }
+        [Foreign Key("CategoryId")]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
     }
 }
