72cddcabd4eb9f19f42c27a858075df67dedc340
Madi Erickson <emadisom@byu.edu>
2025-02-24 15:15:01 -0700

    save

--- Mission8_Section04Group2/Models/Category.cs
+++ Mission8_Section04Group2/Models/Category.cs
@@ -7,7 +7,7 @@ namespace Mission8_Section04Group2.Models
     {
         [Key]
         public int CategoryId { get; set; }
-
+        //here is a comment
         public string Category {  get; set; }
     }
 }
